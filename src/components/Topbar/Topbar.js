10eb93935876a31d073edf7eaf61a56cc886f9af
ArmandoAceves <145027402+ArmandoAceves@users.noreply.github.com>
2024-06-19 15:57:56 -0600

    crm

--- src/components/Topbar/Topbar.js
+++ src/components/Topbar/Topbar.js
@@ -1,9 +1,8 @@
 import React, { useState } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
-import SidebarLat from '../Sidebar/SidebarLat'; // Asegúrate de que la ruta sea correcta
-import { LogoutButton } from '../Logout/Logout';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './Topbar.css';
+import { LogoutButton } from '../Logout';
+import Sidebar1 from '../Sidebar/Sidebar1';
 
 const Topbar = () => {
   const { user } = useAuth0();
@@ -14,31 +13,26 @@ const Topbar = () => {
   };
 
   return (
-    <BrowserRouter>
-      <div className="topbar">
-        <Routes>
-          <Route path="/" element={<SidebarLat />} />
-        </Routes>
-        <img src={user.picture} alt={user.name} onClick={toggleDropdown} className="profile-picture"/>
-        {showDropdown && (
-          <div className="dropdown-container">
-            <div className="dropdown-content">
-              <img src={user.picture} alt={user.name} className="imagen_usuario"/>
-              <p>{user.email}</p>
-              <hr className="mb-3 mx-3 border-top-1 border-none surface-border" />
-              <div className="icons-container">
-                <i className="pi pi-comment" /> 
-                <i className="pi pi-envelope" /> 
-                <i className="pi pi-calendar" />
-              </div>
-              <LogoutButton/>
+    <div className="topbar">
+      <Sidebar1 />
+      <img src={user.picture} alt={user.name} onClick={toggleDropdown} className="profile-picture" />
+      {showDropdown && (
+        <div className="dropdown-container">
+          <div className="dropdown-content">
+            <img src={user.picture} alt={user.name} className="imagen_usuario" />
+            <p>{user.email}</p>
+            <hr className="mb-3 mx-3 border-top-1 border-none surface-border" />
+            <div className="icons-container">
+              <i className="pi pi-comment" />
+              <i className="pi pi-envelope" />
+              <i className="pi pi-calendar" />
             </div>
+            <LogoutButton />
           </div>
-        )}
-      </div>
-    </BrowserRouter>
+        </div>
+      )}
+    </div>
   );
 };
 
 export default Topbar;
-
