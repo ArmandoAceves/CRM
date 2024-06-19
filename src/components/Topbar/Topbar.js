c290c8c8b9a59f1d5609d387106b19752834aff4
ArmandoAceves <145027402+ArmandoAceves@users.noreply.github.com>
2024-06-19 15:48:15 -0600

    crm

--- src/components/Topbar/Topbar.js
+++ src/components/Topbar/Topbar.js
@@ -1,20 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
+import SidebarLat from '../Sidebar/SidebarLat';
+import { LogoutButton } from '../Logout/Logout';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './Topbar.css';
-import { LogoutButton } from '../Logout';
-import Sidebar1 from '../Sidebar/Sidebar1';
-
 
 const Topbar = () => {
   const { user } = useAuth0();
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const toggleDropdown = () => {
+    setShowDropdown(!showDropdown);
+  };
 
   return (
-    <div className="topbar">
-      <Sidebar1/>
-      <LogoutButton/>
-      <span className='hello'>Bienvenido, {user.name}</span>
-      <img src={user.picture} alt={user.name} />
-    </div>
+    <BrowserRouter>
+      <div className="topbar">
+        <Routes>
+          <Route path="/" element={<SidebarLat />} />
+        </Routes>
+        <img src={user.picture} alt={user.name} onClick={toggleDropdown} className="profile-picture"/>
+        {showDropdown && (
+          <div className="dropdown-container">
+            <div className="dropdown-content">
+              <img src={user.picture} alt={user.name} className="imagen_usuario"/>
+              <p>{user.email}</p>
+              <hr className="mb-3 mx-3 border-top-1 border-none surface-border" />
+              <div className="icons-container">
+                <i className="pi pi-comment" /> 
+                <i className="pi pi-envelope" /> 
+                <i className="pi pi-calendar" />
+              </div>
+              <LogoutButton/>
+            </div>
+          </div>
+        )}
+      </div>
+    </BrowserRouter>
   );
 };
 
